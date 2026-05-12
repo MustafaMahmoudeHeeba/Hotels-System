b0197703dfb3c416d574aff55ca6ebf1b2e94251
MustafaMahmoud <awdm6410@gmail.com>
2026-05-12 16:01:55 +0300

    Delete Hotels System/Back directory

--- Hotels System/Back/hotel-service/src/main/java/com/aydin/hotelservice/repository/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aydin.hotelservice.repository;
-
-import com.aydin.hotelservice.model.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel, Long> {
-}
